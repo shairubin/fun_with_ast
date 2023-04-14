874822e27ae2ac6591495b0be02d0ec2f95dc1c4
Shai Rubin <shai@ownbackup.com>
2023-04-14 18:49:34 +0300

    constant source match in a different file

--- fun_with_ast/source_match.py
+++ fun_with_ast/source_match.py
@@ -9,6 +9,7 @@ import re
 #from fun_with_ast.source_match import NumSourceMatcher, BoolSourceMatcher
 
 import create_node
+from constant_source_match import ConstantSourceMatcher
 from fun_with_ast import node_tree_util
 
 from fun_with_ast.create_node import SyntaxFreeLine
@@ -1658,36 +1659,6 @@ def get_TryExcept_expected_parts():
 
 
 # python 3 matching for ast.Constant
-
-class ConstantSourceMatcher():
-    def __init__(self, node, starting_parens=None):
-        if not isinstance(node, ast.Constant):
-            raise ValueError
-        self.constant_node = node
-        from fun_with_ast.str_source_match import StrSourceMatcher
-        self.str_matcher = StrSourceMatcher(node, starting_parens)
-        from fun_with_ast.num_source_match import NumSourceMatcher, BoolSourceMatcher
-        self.num_matcher = NumSourceMatcher(node, starting_parens)
-        self.bool_matcher = BoolSourceMatcher(node, starting_parens)
-
-    def Match(self, string):
-        if isinstance(self.constant_node.n, bool):
-            return self.bool_matcher.Match(string)
-        if isinstance(self.constant_node.n, int) and isinstance(self.constant_node.s, int):
-            return self.num_matcher.Match(string)
-        if isinstance(self.constant_node.n, str) and isinstance(self.constant_node.s, str):
-            return self.str_matcher.Match(string)
-        raise NotImplementedError
-
-    def GetSource(self):
-        if isinstance(self.constant_node.n, bool) and isinstance(self.constant_node.s, int):
-            return self.bool_matcher.GetSource()
-        if isinstance(self.constant_node.n, int) and isinstance(self.constant_node.s, int):
-            return self.num_matcher.GetSource()
-        if isinstance(self.constant_node.n, str) and isinstance(self.constant_node.s, str):
-            return self.str_matcher.GetSource()
-
-        raise NotImplementedError
 
 
 class TryFinallySourceMatcher(DefaultSourceMatcher):

cccb03d118e8f2117280e04e7a95ba24668a8661
Shai Rubin <shai@ownbackup.com>
2023-05-05 16:06:17 +0300

    build new version script

--- devops/build_new_version.sh
+++ devops/build_new_version.sh
@@ -5,8 +5,9 @@ ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 echo "patch a version"
 poetry version patch
 echo "adding commit message"
+git add pyproject.toml
 git commit -m 'new patch version update'
-echo "push to git"
-git push
+echo "push to git. Enter user name and your access token"
+git push origin main
 echo "poetry build"
 poetry build

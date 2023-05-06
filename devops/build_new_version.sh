d4d4e2bb3da5536fd5bdce53e464a3f18442bd24
Shai Rubin <shai@ownbackup.com>
2023-05-06 10:09:19 +0300

    fix auto build version

--- devops/build_new_version.sh
+++ devops/build_new_version.sh
@@ -9,5 +9,12 @@ git add pyproject.toml
 git commit -m 'new patch version update'
 echo "push to git. Enter user name and your access token"
 git push origin main
+if [ $? -eq 0 ]
+then
+  echo "Successfully push"
+else 
+  echo "Could not git push" >&2
+  exit 1
+fi
 echo "poetry build"
 poetry build

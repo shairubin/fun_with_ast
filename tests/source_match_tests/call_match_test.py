b67a7b18aa8f9b786b8a49cb59ac86db8d9b2581
shai <shai_rubin@yahoo.com>
2023-06-30 18:02:38 +0300

    call match tests

--- tests/source_match_tests/call_match_test.py
+++ tests/source_match_tests/call_match_test.py
@@ -2,41 +2,50 @@ import unittest
 
 from fun_with_ast.manipulate_node import create_node as create_node
 from fun_with_ast.source_matchers.matcher_resolver import GetDynamicMatcher
+from tests.source_match_tests.base_test_utils import BaseTestUtils
 
 
-class CallMatcherTest(unittest.TestCase):
+class CallMatcherTest(BaseTestUtils):
 
     def testBasicMatch(self):
         node = create_node.Call('a')
         string = 'a()'
-        matcher = GetDynamicMatcher(node)
-        matcher.do_match(string)
-        self.assertEqual(string, matcher.GetSource())
+        self._verify_match(node, string)
+    def testBasicMatchWS(self):
+        node = create_node.Call('a')
+        string = ' a()'
+        self._verify_match(node, string)
 
+    def testBasicMatchWS2(self):
+        node = create_node.Call('a.b')
+        string = ' a.b()'
+        self._verify_match(node, string)
     def testMatchStarargs(self):
         node = create_node.Call('a', starargs='args')
         string = 'a(*args)'
-        matcher = GetDynamicMatcher(node)
-        matcher.do_match(string)
-        self.assertEqual(string, matcher.GetSource())
+        self._verify_match(node, string)
 
     def testMatchWithStarargsBeforeKeyword(self):
         node = create_node.Call('a', keywords=[create_node.keyword('b', 'c')], starargs='args')
         string = 'a(*args, b=c)'
-        matcher = GetDynamicMatcher(node)
-        matcher.do_match(string)
-        self.assertEqual(string, matcher.GetSource())
+        self._verify_match(node, string)
 
     def testCallWithAttribute(self):
         node = create_node.Call('a.b')
         string = 'a.b()'
-        matcher = GetDynamicMatcher(node)
-        matcher.do_match(string)
-        self.assertEqual(string, matcher.GetSource())
+        self._verify_match(node, string)
 
     def testCallWithAttributeAndParam(self):
         node = create_node.Call('a.b', args=[create_node.Str('fun-with-ast')])
         string = 'a.b(\'fun-with-ast\')'
-        matcher = GetDynamicMatcher(node)
-        matcher.do_match(string)
-        self.assertEqual(string, matcher.GetSource())
+        self._verify_match(node, string)
+
+    def testCallWithAttributeAndParam2(self):
+        node = create_node.Call('a.b', args=[create_node.Num('1')])
+        string = '(a.b(1))'
+        self._verify_match(node, string)
+
+    def testCallWithAttributeAndParamWS(self):
+        node = create_node.Call('a.b', args=[create_node.Str('fun-with-ast')])
+        string = 'a.b(\'fun-with-ast\')'
+        self._verify_match(node, string)

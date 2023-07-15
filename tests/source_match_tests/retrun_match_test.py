9979215da5c828c898a5e676cf6485d8cc80a5c9
shai <shai_rubin@yahoo.com>
2023-07-15 18:28:58 +0300

    tests with GetNodeFromInput

--- tests/source_match_tests/retrun_match_test.py
+++ tests/source_match_tests/retrun_match_test.py
@@ -1,15 +1,27 @@
 import unittest
 
 import pytest
+from fun_with_ast.manipulate_node.get_node_from_input import GetNodeFromInput
 
 from fun_with_ast.manipulate_node import create_node
 from fun_with_ast.source_matchers.matcher_resolver import GetDynamicMatcher
+from tests.source_match_tests.base_test_utils import BaseTestUtils
 
 
-class RetrunMatcherTest(unittest.TestCase):
+class RetrunMatcherTest(BaseTestUtils):
     def testSimpleReturn(self):
         node = create_node.Return(1)
         string = 'return 1'
+        self._assert_match(node, string)
+
+    def testSimpleReturnFromString(self):
+        string = 'return 1'
+        node = GetNodeFromInput(string)
+        self._assert_match(node, string)
+
+    def testSimpleReturnFromString2(self):
+        string = 'return  isPerfectSquare(5 * n * n + 4) or isPerfectSquare(5 * n * n - 4)'
+        node = GetNodeFromInput(string)
         self._assert_match(node, string)
 
 

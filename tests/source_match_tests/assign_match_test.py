00730767349352272ffbd36d0b4abbd428b6588b
shai <shai_rubin@yahoo.com>
2023-08-19 09:31:49 +0300

    added tests that shows failure

--- tests/source_match_tests/assign_match_test.py
+++ tests/source_match_tests/assign_match_test.py
@@ -1,6 +1,8 @@
 import unittest
 
 import pytest
+
+from fun_with_ast.manipulate_node.get_node_from_input import GetNodeFromInput
 from fun_with_ast.source_matchers.exceptions import BadlySpecifiedTemplateError
 
 from fun_with_ast.manipulate_node import create_node
@@ -121,6 +123,15 @@ class AssignMatcherTest(BaseTestUtils):
         with pytest.raises(BadlySpecifiedTemplateError):
             matcher.do_match(string)
 
+    def testAssignFromSource(self):
+        string = 'a=1'
+        node = GetNodeFromInput(string)
+        self._verify_match(node, string)
+
+    def testAnnAssignFromSource(self):
+        string = 'a: int =1'
+        node = GetNodeFromInput(string)
+        self._verify_match(node, string)
 
     def _assert_matched_source(self, node, string):
         self._verify_match(node, string)

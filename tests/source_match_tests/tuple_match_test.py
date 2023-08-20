75d3b72ae7facc187c6319cafce21c1c70c4e37f
shai <shai_rubin@yahoo.com>
2023-08-20 16:30:04 +0300

    support tuple like 1, or (1,)

--- tests/source_match_tests/tuple_match_test.py
+++ tests/source_match_tests/tuple_match_test.py
@@ -3,6 +3,7 @@ import unittest
 import pytest
 
 from fun_with_ast.manipulate_node import create_node
+from fun_with_ast.manipulate_node.get_node_from_input import GetNodeFromInput
 from fun_with_ast.source_matchers.exceptions import BadlySpecifiedTemplateError
 from fun_with_ast.source_matchers.matcher_resolver import GetDynamicMatcher
 from tests.source_match_tests.base_test_utils import BaseTestUtils
@@ -31,22 +32,28 @@ class TupleTest(BaseTestUtils):
         string = '\t a,\t\tb \t #comment'
         self._assert_match(node, string)
 
-    #@pytest.mark.skip(reason="Not Implemented Yet - illegal tuple ")
     def testBasicTupleNoIllegal(self):
         node = create_node.Tuple(['a', 'b'])
         string = '(\t a,\t\tb \t #comment'
         matcher = GetDynamicMatcher(node)
         with pytest.raises(BadlySpecifiedTemplateError):
             matcher.do_match(string)
-        #self.assertEqual(string, matcher.GetSource())
 
-    # @pytest.mark.skip(reason="Not Implemented Yet - comma without another name")
     def testBasicSingleTuple(self):
         node = create_node.Tuple(['a'])
         string = '(\t   a, \t)'
         self._assert_match(node, string)
+    def testBasicSingleTuple3(self):
+        node = create_node.Tuple(['a'])
+        string = '(a,)'
+        self._assert_match(node, string)
 
-    def testBasicSingleTuple(self):
+    def testBasicSingleTuple4(self):
+        node = create_node.Tuple(['a'])
+        string = 'a,'
+        self._assert_match(node, string)
+
+    def testBasicSingleTuple2(self):
         node = create_node.Tuple(['a'])
         string = '(\t   a \t)'
         self._assert_match(node, string)
@@ -59,6 +66,30 @@ class TupleTest(BaseTestUtils):
     def testTupleWithCommentAndWSAndConst(self):
         node = create_node.Tuple(['a', 1])
         string = ' (\t   a\t, 1 \t) \t #comment'
+        self._assert_match(node, string)
+    def testCreateNodeFromInput(self):
+        string = '(\t   a\t, 1 \t) \t #comment'
+        node =GetNodeFromInput(string)
+        self._assert_match(node, string)
+    def testCreateNodeFromInput2(self):
+        string = '(a,)'
+        node =GetNodeFromInput(string)
+        self._assert_match(node, string)
+    def testCreateNodeFromInput3(self):
+        string = '(a,  \t b,   )'
+        node =GetNodeFromInput(string)
+        self._assert_match(node, string)
+    def testCreateNodeFromInput4(self):
+        string = 'a,  \t b,   '
+        node =GetNodeFromInput(string)
+        self._assert_match(node, string)
+    def testCreateNodeFromInput5(self):
+        string = '-1,'
+        node =GetNodeFromInput(string)
+        self._assert_match(node, string)
+    def testCreateNodeFromInput6(self):
+        string = '(-1,)'
+        node =GetNodeFromInput(string)
         self._assert_match(node, string)
 
     def _assert_match(self, node, string):

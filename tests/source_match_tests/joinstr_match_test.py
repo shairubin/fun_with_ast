a7ca794a947a38e4dc5f7a27e8e25f089915e6be
shai <shai_rubin@yahoo.com>
2023-12-18 16:58:50 +0200

    next test ready to test

--- tests/source_match_tests/joinstr_match_test.py
+++ tests/source_match_tests/joinstr_match_test.py
@@ -58,19 +58,23 @@ class JoinStrMatcherTests(BaseTestUtils):
         string = "f'{X}'"
         self._verify_match(node, string)
 
-    @pytest.mark.skip("new implementation - not supported yet")
+    #@pytest.mark.skip("new implementation - not supported yet")
     def testBasicMatchFromInput55(self):
         node = GetNodeFromInput("f\"{X}\"")
         string = "f\"{X}\""
         self._verify_match(node, string)
 
-    @pytest.mark.skip("new implementation - not supported yet")
+    #@pytest.mark.skip("new implementation - not supported yet")
     def testBasicMatchFromInput56(self):
         node = GetNodeFromInput("f\"{X}\"")
         string = "(f\"{X}\")"
         self._verify_match(node, string)
+    def testBasicMatchFromInput56_1(self):
+        node = GetNodeFromInput("f\"{X }\"")
+        string = "(f\"{X }\")"
+        self._verify_match(node, string)
 
-    @pytest.mark.skip("new implementation - not supported yet")
+    #@pytest.mark.skip("new implementation - not supported yet")
     def testBasicMatchFromInput57(self):
         node = GetNodeFromInput("f'{X}'")
         string = "(f'{X}')"

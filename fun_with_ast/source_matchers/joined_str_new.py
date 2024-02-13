f5a38f0bf518a996105c4f969df216d687db9b61
shai <shai_rubin@yahoo.com>
2024-02-13 17:40:55 +0200

    added two tests small fix to jstr matcher

--- fun_with_ast/source_matchers/joined_str_new.py
+++ fun_with_ast/source_matchers/joined_str_new.py
@@ -165,7 +165,7 @@ class JoinedStrSourceMatcherNew(DefaultSourceMatcher):
         if not isinstance(format_value_node, ast.FormattedValue):
             raise ValueError('value node is not FormattedValue')
         value_node = format_value_node.value
-        if not isinstance(value_node, (ast.Name, ast.Constant, ast.Attribute, ast.Subscript , ast.Call)):
+        if not isinstance(value_node, (ast.ListComp, ast.BinOp,  ast.Name, ast.Constant, ast.Attribute, ast.Subscript , ast.Call)):
             raise NotImplementedError('only Name , Constant, Attribute and Call nodes are supported')
 
         stripped_format  = GetSource(value_node, text=field_name_part[0])

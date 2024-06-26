cf2ee1d2a53cc4c94e41f21ec18c3c11851986ff
shai <shai_rubin@yahoo.com>
2024-06-26 13:35:13 +0300

    code review comments

--- tests/manipulate_tests/module_body_remove_manipulation_test.py
+++ tests/manipulate_tests/module_body_remove_manipulation_test.py
@@ -1,5 +1,4 @@
 import ast
-import sys
 
 import pytest
 
@@ -7,7 +6,6 @@ from fun_with_ast.get_source import GetSource
 from fun_with_ast.manipulate_node.body_manipulator import BodyManipulator
 from fun_with_ast.manipulate_node.get_node_from_input import GetNodeFromInput
 from fun_with_ast.source_matchers.matcher_resolver import GetDynamicMatcher
-from tests.manipulate_tests.base_test_utils_manipulate import bcolors
 
 input_legend = ('inject-source', 'location', 'original-if', 'expected', 'match-expected', 'injected_second_source')
 
@@ -49,49 +47,3 @@ class TestRemoveNodeManipulation:
             body_manipulator.remove_node(node_index, removed_source)
             source_after_remove = GetSource(module_node)
             assert source_after_remove == expected
-
-    #
-    # def _create_nodes(self, capsys, injected_source, original_source, injected_second_source='', is_module=False):
-    #     self._capture_source(capsys, original_source, 'original source:', bcolors.OKBLUE)
-    #     if_node = self._create_if_node(original_source, is_module)
-    #     injected_node, injected_node_source = self._create_injected_node(injected_source, injected_second_source)
-    #     return if_node, injected_node
-
-    # def _create_injected_node(self, injected_source, injected_second_source):
-    #     injected_node_source = injected_source
-    #     #module = False
-    #     #if injected_second_source:
-    #     injected_node_source +=  injected_second_source
-    #     #    module=True
-    #     injected_node = GetNodeFromInput(injected_node_source, get_module=True)
-    #     injected_node_matcher = GetDynamicMatcher(injected_node)
-    #     injected_node_matcher.do_match(injected_node_source)
-    #     source_from_matcher = injected_node_matcher.GetSource()
-    #     assert source_from_matcher == injected_node_source
-    #     source_from_get_source = GetSource(injected_node, assume_no_indent=True)
-    #     assert source_from_get_source == injected_node_source
-    #     return injected_node, injected_node_source
-
-    # def _create_if_node(self, original_source, is_module):
-    #     node = GetNodeFromInput(original_source, get_module=is_module)
-    #     node_matcher = GetDynamicMatcher(node)
-    #     node_matcher.do_match(original_source)
-    #     if_node_source = node_matcher.GetSource()
-    #     assert if_node_source == original_source
-    #     # if_node.matcher = if_node_matcher
-    #     return node
-
-    def _capture_source(self, capsys, source, title, color, ignore_ident=False):
-        if not ignore_ident:
-            try:
-                compile(source, '<string>', mode='exec')
-            except Exception as e:
-                assert False, f'Error in source compilation'
-        print(color + '\n' + title + '\n' + source + bcolors.ENDC)
-        out, _ = capsys.readouterr()
-        sys.stdout.write(out)
-
-    def _source_after_composition(self, if_node, capsys):
-        composed_source = GetSource(if_node, assume_no_indent=True)
-        self._capture_source(capsys, composed_source, 'Modified source', bcolors.OKCYAN)
-        return composed_source

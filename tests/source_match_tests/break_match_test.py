4b95e6970edcd355fd6ffaa6da2e51f1e5a44654
shai <shai_rubin@yahoo.com>
2024-09-03 16:53:39 +0300

    test fail on 3.11

--- tests/source_match_tests/break_match_test.py
+++ tests/source_match_tests/break_match_test.py
@@ -1,3 +1,5 @@
+import sys
+
 from fun_with_ast.manipulate_node.get_node_from_input import GetNodeFromInput
 from tests.source_match_tests.base_test_utils import BaseTestUtils
 
@@ -23,3 +25,9 @@ class BreakTest(BaseTestUtils):
             """
         node = GetNodeFromInput(string, get_module=True)
         self._verify_match(node, string)
+    #@pytest.mark.skipif(sys.version_info != (3, 10), reason="requires python3.10 or higher")
+    def testexamplefail_311_only(self):
+
+        print(sys.version_info)
+        assert 1 == 2
+
